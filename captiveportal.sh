db2ff74cbae60b53b17b99fb7bd0c030fcc687ba
tretos53 <adam.g@outlook.com>
2018-09-15 17:14:05 +0100

    Update captiveportal.sh

--- captiveportal.sh
+++ captiveportal.sh
@@ -1,83 +1,77 @@
 #!/bin/bash
-#
-# From 
-# https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390
-# https://andrewwippler.com/2016/03/11/wifi-captive-portal/
-# https://www.raspberrypi.org/forums/viewtopic.php?t=161715
-# and other places.
 
 if [ "$EUID" -ne 0 ]
 	then echo "Must be root, run sudo -i before running that script."
 	exit
 fi
 
-echo "┌───────────────────────────────────┐"
-echo "|This script might take a while,	  |"
-echo "|so if you dont see much progress,  |"
-echo "|wait till you see all done message.|"
-echo "└───────────────────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|This script might take a while,"
+echo "|so if you dont see much progress,"
+echo "|wait till you see --all done-- message."
+echo "└─────────────────────────────────────────"
 read -p "Press enter to continue"
 
-echo "┌─────────────────────┐"
-echo "|Updating repositories|"
-echo "└─────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Updating repositories"
+echo "└─────────────────────────────────────────"
 apt-get update -yqq
 
-# echo "┌───────────────────────────────────────────┐"
+# echo "┌─────────────────────────────────────────"
 # echo "|Upgrading packages, this might take a while|"
-# echo "└───────────────────────────────────────────┘"
+# echo "└─────────────────────────────────────────"
 # apt-get upgrade -yqq
 
-echo "┌────────────────────────────────┐"
-echo "|Installing and configuring nginx|"
-echo "└────────────────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Installing and configuring nginx"
+echo "└─────────────────────────────────────────"
 apt-get install nginx -yqq
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/default_nginx -O /etc/nginx/sites-enabled/default
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/index.php -O  /var/www/html/index.php
 
-echo "┌──────────────────┐"
-echo "|Installing dnsmasq|"
-echo "└──────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Installing dnsmasq"
+echo "└─────────────────────────────────────────"
 apt-get install dnsmasq -yqq
 
-echo "┌──────────────────┐"
-echo "|Configuring wlan0 |"
-echo "└──────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Configuring wlan0"
+echo "└─────────────────────────────────────────"
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/dhcpcd.conf -O /etc/dhcpcd.conf
 
-echo "┌────────────────────┐"
-echo "|Configuring dnsmasq |"
-echo "└────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Configuring dnsmasq"
+echo "└─────────────────────────────────────────"
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/dnsmasq.conf -O /etc/dnsmasq.conf
 
-echo "┌─────────────────────────────────────────┐"
-echo "|configuring dnsmasq to start at boot	|"
-echo "└─────────────────────────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|configuring dnsmasq to start at boot"
+echo "└─────────────────────────────────────────"
 update-rc.d dnsmasq defaults
 
-echo "┌──────────────────┐"
-echo "|Installing hostapd|"
-echo "└──────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Installing hostapd"
+echo "└─────────────────────────────────────────"
 apt-get install hostapd -yqq
 
-echo "┌────────────────────┐"
-echo "|Configuring hostapd |"
-echo "└────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Configuring hostapd"
+echo "└─────────────────────────────────────────"
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/hostapd.conf -O /etc/hostapd/hostapd.conf
 sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
 
-echo "┌─────────────────────────────────────────┐"
-echo "|configuring hostapd to start at boot	|"
-echo "└─────────────────────────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|configuring hostapd to start at boot"
+echo "└─────────────────────────────────────────"
 update-rc.d hostapd defaults
 
-echo "┌─────────────────────────────────┐"
-echo "|After the next step is complete,	|"
-echo "|please reboot your pi and test.	|"
-echo "└─────────────────────────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|After the next step is complete,"
+echo "|please reboot your pi and test."
+echo "└─────────────────────────────────────────"
 read -p "Press enter to install PHP"
 
-echo "┌───────────────┐"
-echo "|Installing PHP7|"
-echo "└───────────────┘"
+echo "┌─────────────────────────────────────────"
+echo "|Installing PHP7"
+echo "└─────────────────────────────────────────"
 apt-get install php7.0-fpm -yqq
